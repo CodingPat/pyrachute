1fccb6867f6a66d17572f0a05ec66470af239b3f
Patrick Dudjalija <patrick.dudjalija@gmail.com>
2019-06-03 21:49:08 +0200

    déplacement bateau + score

--- pyrachute.py
+++ pyrachute.py
@@ -4,8 +4,10 @@ import random
 WIDTH=800
 HEIGHT=600
 DELAY=3
-SPEED=5
-MARGINTOP=50
+SPEEDPARACHUTE=5
+SPEEDBATEAU=4
+MARGEHAUT=50
+MARGEBAS=150
 WIDTHPARACHUTE=200
 
 def create_parachute():
@@ -16,28 +18,49 @@ def create_parachute():
     
 
 def update():
+    global score
     for index,parachute in enumerate(parachutes):
-        parachute.y+=SPEED
-        if parachute.bottom==0:
+        parachute.y+=SPEEDPARACHUTE
+        if parachute.colliderect(bateau):
+            score+=1
+            parachutes.pop(index)
+        elif parachute.bottom==HEIGHT-MARGEBAS:
             parachutes.pop(index)
 
+    
 
+    if keyboard.left:
+        bateau.image='boatleft200px'
+        bateau.x-=SPEEDPARACHUTE
+    elif keyboard.right:
+        bateau.image='boatright200px'
+        bateau.x+=SPEEDPARACHUTE
+        
+    if bateau.left<0:
+        bateau.left=0
+    elif bateau.right>WIDTH:
+        bateau.right=WIDTH
+
+    
+        
 def draw():
     beach.draw()
-    boat.draw()
+    bateau.draw()
     for parachute in parachutes:
         parachute.draw()
+    screen.draw.text("Score : {}".format(score),(550,0),fontsize=60)
 
 
 beach=Actor('beach800px',anchor=('center','bottom'))
-boat=Actor('boat200px')
-boat.pos=(WIDTH/2,HEIGHT-150)
+bateau=Actor('boatright200px')
+bateau.pos=(WIDTH/2,HEIGHT-150)
 
 parachutes=[]
 
-
+score=0
 
 clock.schedule_interval(create_parachute,DELAY)
+
 
 
 pgzrun.go()
